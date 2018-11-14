3c04b2d28223cc8c3508e1e9292a961cfa1b8025
danielbc09 <jdanielbc193@gmail.com>
2018-11-15 00:27:47 +0100

    closes #5

--- src/test/java/cc/hito_2/ejercicios/cc_hito_2_ejercicios/controller/NaufragioControllerTest.java
+++ src/test/java/cc/hito_2/ejercicios/cc_hito_2_ejercicios/controller/NaufragioControllerTest.java
@@ -7,14 +7,9 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
 import java.util.Arrays;
-
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
